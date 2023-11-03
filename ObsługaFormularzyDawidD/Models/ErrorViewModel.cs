988fd5a111bce64320ad7b55fb30fe95928eb9c0
HP <HP@LAPTOP-UC86IDGV>
2023-11-03 15:40:40 +0100

    Dodaj pliki projektów.

---
+++ ObsługaFormularzyDawidD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObsługaFormularzyDawidD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
